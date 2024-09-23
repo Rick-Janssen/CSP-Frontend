49fa27a65c61cb6e15ce8e96fc475a336a60cffa
Stefan Mars <stefanrmars@gmail.com>
2024-09-23 12:09:52 +0200

    added productList

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Header from "./components/Header/header";
 import Navbar from "./components/Navbar/Navbar";
 import ProductList from "./components/ProductList/ProductList";
 
-const App = () =>{
-  return ( 
-     <>
-      <Navbar/>
-      <Header/>
-      <ProductList/>
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <ProductList />
     </>
 
-   );
+  );
 
 }
 export default App;
