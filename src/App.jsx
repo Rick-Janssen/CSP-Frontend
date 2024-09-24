ee00375a3e3315340d820a940a2216c1c8dbf98e
Rick-Janssen <rick.janssen3107@gmail.com>
2024-09-24 11:49:40 +0200

    login pain

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/header";
 import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./pages/HomePage/HomePage"
+import HomePage from "./pages/HomePage/HomePage";
+import Register from "./pages/Login/Register";
 import ProductList from "./components/ProductList/ProductList";
 
 const App = () =>{
   return ( 
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Register />} /> 
     </Routes>
 
   );
