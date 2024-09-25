e0fff53cd00b4117420776183c392aa6faa2d476
Stefan Mars <stefanrmars@gmail.com>
2024-09-25 12:14:40 +0200

    checklogin with logout/ login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Header from "./components/Header/header";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
 import Register from "./pages/Login/Register";
+
 import Login from "./pages/Login/Login";
+
 import ProductList from "./components/ProductList/ProductList";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 import LandingPage from "./components/LandingPage/LandingPage";
@@ -12,19 +14,20 @@ import DetailsPage from "./components/DetailsPage/DetailsPage";
 
 const App = () => {
   return (
-        <>
+    <>
       <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<HomePage />} />
-          <Route path="product/:id" element={<DetailsPage />} />
-          <Route element={<ProtectedRoutes />}>
-            {/* <Route path="/admin" element={<Admin />} /> */}
-          </Route>
-        </Routes>
-        </> 
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/home" element={<HomePage />} />
+        <Route path="product/:id" element={<DetailsPage />} />
+        <Route element={<ProtectedRoutes />}>
+          {/* <Route path="/admin" element={<Admin />} /> */}
+        </Route>
+      </Routes>
+    </>
 
   );
 
